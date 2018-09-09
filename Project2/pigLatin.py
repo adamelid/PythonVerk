e619d03cec60a035b45b2925810d37d4b58852d3
adamelid <adamelid16@gmail.com>
2018-09-09 17:59:10 +0000

    Still cleaning up

--- Project2/pigLatin.py
+++ Project2/pigLatin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 while True:
 
     consonants = "bcdfghjklmnpqrstvxzwy"
@@ -22,27 +21,4 @@ while True:
         newString += consonantAdd
         print(newString)
     else:
-=======
-while True:
-
-    consonants = "bcdfghjklmnpqrstvxzwy"
-    vowels = "aeiou"
-    consonantAdd = "ay"
-    vowelAdd = "yay"
-    stringInput = input("Enter a word: ")
-    newString = stringInput
-    
-    if stringInput.lower() == ".":
-        exit()
-    elif stringInput[0].lower() in vowels:
-        stringInput += vowelAdd
-        print(stringInput)
-    elif stringInput[0].lower() in consonants:
-        for i in newString:
-            if newString[0] not in vowels:
-                newString = newString[1::] + newString[0]
-            else:
-                break
-    else:
->>>>>>> f81828adbe9a1c17600731c2ed60312dda40508c
         print("Words start with letters...")
